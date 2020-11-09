54844628fc0b80bd7888db45126e6ddd72876600
Warren Wong <Warren.Wong4@gmail.com>
2020-11-09 13:30:37 -0500

    changes

--- week-2/day-4/script.js
+++ week-2/day-4/script.js
@@ -1,11 +1,24 @@
 var currentPosition = 0;
 
 var prevImage = function () {
-   currentPosition = currentPosition + 500;
-   document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
-}         
+    currentPosition = currentPosition + 500;
+    document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
+ }         
                  
 var nextImage = function () {
-    currentPosition = currentPosition - 500;        
-    document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
-}                
+     currentPosition = currentPosition - 500;        
+     document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
+}             
+
+// var prevImage = function(currentPosition) {
+//     for(let currentPosition = 0; currentPosition <= 2000; currentPosition = currentPosition + 500){
+//         document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
+//     }
+// }
+
+// var nextImage = function(currentPosition){
+//     for(let currentPosition = 0; currentPosition <= -2000; currentPosition = currentPosition - 500){
+//         document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
+//     }
+// }
+// Add some conditional logic to your javascript code to prevent users from translating the imageSlider too far in either direction
