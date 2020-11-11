6bd178fc6391f837b017a29da65a8fd465973e54
Warren Wong <Warren.Wong4@gmail.com>
2020-11-10 20:00:52 -0500

    changes

--- week-2/day-4/script.js
+++ week-2/day-4/script.js
@@ -1,20 +1,25 @@
 var currentPosition = 0;
 
-var prevImage = function () {
-    currentPosition = currentPosition + 500;
-    document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
- }         
+// var prevImage = function () {
+//     currentPosition = currentPosition + 500;
+//     document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
+//  }         
                  
-var nextImage = function () {
-     currentPosition = currentPosition - 500;        
-     document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
+function nextImage() {
+     if(currentPosition > 1000){
+          console.log(currentPosition);
+          currentPosition +=500;
+          document.getElementById('imageSlider2').style.transform = `translate(${currentPosition}px)`;
+     }
+    
 }             
 
-// var prevImage = function(currentPosition) {
-//     for(let currentPosition = 0; currentPosition <= 2000; currentPosition = currentPosition + 500){
-//         document.getElementById('imageSlider2').style.transform ='translateX(' + currentPosition + 'px)';
-//     }
-// }
+function prevImage() {
+     if(currentPosition > -1000) {
+          console.log(currentPosition);
+          currentPosition -= 500;
+          document.getElementById('imageSlider2').style.transform = `translate(${currentPosition}px)`;
+     }
 
 // var nextImage = function(currentPosition){
 //     for(let currentPosition = 0; currentPosition <= -2000; currentPosition = currentPosition - 500){
